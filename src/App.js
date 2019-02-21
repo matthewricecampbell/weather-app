0f669a3eb8e1d0f0f3b31fa4dc57d6101fb21c9f
Matt Campbell <matthewricecampbell@gmail.com>
2019-02-21 13:00:25 -0500

    Add react-router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
 import WeatherForm from './components/WeatherForm';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Weather Time!</h2>
-          <div className="row">
-            <WeatherForm flexDirection={'row'}/>
-          </div>
-        </div>
-        <Home/>
-      </div>
-    );
-  }
-}
 
-export default App;
+const Forcast = () => (
+  <div>
+    <h2>Forcast</h2>
+  </div>
+)
+
+const App = () => (
+  <Router>
+     <div className="App">
+       <div className="App-header">
+         <h2>Weather Time!</h2>
+         <div className="row">
+           <WeatherForm flexDirection={'row'}/>
+         </div>
+       </div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/forcast" component={Forcast}/>
+    </div>
+  </Router>
+)
+export default App 
