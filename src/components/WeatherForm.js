1abd79bfe994d23eeed29a0c6d1f4838bbe087cc
Matt Campbell <matthewricecampbell@gmail.com>
2019-02-21 11:50:02 -0500

    Set city name to state

--- src/components/WeatherForm.js
+++ src/components/WeatherForm.js
@@ -3,12 +3,52 @@ import PropTypes from 'prop-types';
 import '../App.css';
 
 class WeatherForm extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      city: 'Chattanooga, Tennessee',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('state', this.state)
+  }
+
+  handleChange(event) {
+    var value = event.target.value;
+
+    this.setState(function () {
+      return {
+        city: value
+      }
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state.city)
+  }
+
   render() {
     return(
-      <div className={this.props.flexDirection}>
-        <input type="text" placeholder="Chattanooga, Tennessee" />
-        <button type="button" className="btn-primary">Get Weather</button>
-      </div>
+      <form className={this.props.flexDirection} onSubmit={this.handleSubmit}>
+        <input
+          id='city'
+          placeholder='Chattanooga, Tennessee'
+          type='text'
+          value={this.state.city}
+          autoComplete='off'
+          onChange={this.handleChange}
+        />
+        <button
+          className='btn-primary'
+          type='submit'
+          disabled={!this.state.city}>
+            Get Weather 
+        </button>
+      </form>
     )
   }
 }
